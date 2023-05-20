9a19ef4008c8105ccba673592a4f962e36c71a88
ameyabp <ameyap@umd.edu>
2023-05-20 16:40:32 -0500

    Partial changes for expand-contract dropdown for News

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-d3.select("#timestamp")
-    .append("text")
-    .text("Created using bootstrap. Last updated on 7 April 2023.")
+function expandContractNews() {
+    console.log("chaltay baba!")
+    var btn = d3.select("#news-expand-contract")
+    
+    console.log(btn.node().value)            
+    
+    if (btn.node().value === "contracted") {
+        btn.node().value = "expanded"
+        btn.text("-")
+    }
+    else {
+        btn.node().value = "contracted"
+        btn.text("+")
+    }
+    
+    console.log(btn.node().value)
+
+}
